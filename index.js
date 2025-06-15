ddbaaefdba9930acae879faced32495d995fdb2c
Md. Ikramul Islam <tnmrfsn@gmail.com>
2025-06-16 03:06:17 +0600

    added event listener for buttons

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
+const incrementBtn = document.getElementById("increment-btn");
+const savetBtn = document.getElementById("save-btn");
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
 
-function increment() {
+incrementBtn.addEventListener("click", () => {
   count += 1;
   countEl.textContent = count;
-}
+});
 
-function decrement() {
-  count -= 1;
-  countEl.textContent = count;
-}
-
-function save() {
+savetBtn.addEventListener("click", () => {
   let countStr = count + " , ";
   saveEl.textContent += countStr;
   countEl.textContent = 0;
   count = 0;
-}
+});
